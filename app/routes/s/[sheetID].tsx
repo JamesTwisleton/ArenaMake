fec1573465771ccb6c28fc02a8037aae5d4a8e17
James Twisleton <james@consultwiz.biz>
2023-04-05 22:49:19 +0100

    reading sheet data into the frontend

--- app/routes/s/[sheetID].tsx
+++ app/routes/s/[sheetID].tsx
@@ -6,13 +6,12 @@ const {
 
 import { Handlers, PageProps } from "$fresh/server.ts";
 
-interface User {
-  _id: string;
+interface Sheet {
   sheetID: string;
   title: string;
 }
 
-export const handler: Handlers<User | null> = {
+export const handler: Handlers<Sheet | null> = {
   async GET(_, ctx) {
     const sheetID = ctx.params.sheetID;
     const resp = await fetch(`${APP_BASE_URI}/api/sheet/${sheetID}`);
@@ -20,20 +19,17 @@ export const handler: Handlers<User | null> = {
       return ctx.render(null);
     }
     const response_json = await resp.json();
-    console.log(response_json);
-    return ctx.render({_id:response_json['_id'],sheetID:response_json['sheetID'],title:response_json['title']});
+    return ctx.render({sheetID:response_json['sheetId'],title:response_json['title']});
   },
 };
 
-export default function Page({ data }: PageProps<User | null>) {
+export default function Page({ data }: PageProps<Sheet | null>) {
   if (!data) {
-    return <h1>User not found</h1>;
+    return <h1>Sheet not found!</h1>;
   }
 
   return (
     <div>
-      {/* <img src={data._id} width={64} height={64} /> */}
-      <h1>{data._id}</h1>
       <h1>{data.sheetID}</h1>
       <h1>{data.title}</h1>
     </div>
